33431f77ade977f645a744bd6fc12bcd4f2b1d90
stefano <stefano.defalco@emeal.nttdata.com>
2024-02-12 12:26:06 +0100

    add rest client call for service workflow resource

--- src/main/java/it/gov/pagopa/atmlayer/service/consolebackend/client/ModelWebClient.java
+++ src/main/java/it/gov/pagopa/atmlayer/service/consolebackend/client/ModelWebClient.java
@@ -3,6 +3,9 @@ package it.gov.pagopa.atmlayer.service.consolebackend.client;
 import io.smallrye.mutiny.Uni;
 import it.gov.pagopa.atmlayer.service.consolebackend.clientdto.BpmnVersionFrontEndDTO;
 import it.gov.pagopa.atmlayer.service.consolebackend.clientdto.UserProfileDto;
+import it.gov.pagopa.atmlayer.service.consolebackend.clientdto.WorkflowResourceFrontEndDTO;
+import it.gov.pagopa.atmlayer.service.consolebackend.enums.DeployableResourceType;
+import it.gov.pagopa.atmlayer.service.consolebackend.enums.StatusEnum;
 import it.gov.pagopa.atmlayer.service.consolebackend.model.PageInfo;
 import jakarta.validation.constraints.NotNull;
 import jakarta.ws.rs.*;
@@ -11,6 +14,8 @@ import org.eclipse.microprofile.openapi.annotations.enums.SchemaType;
 import org.eclipse.microprofile.openapi.annotations.media.Schema;
 import org.eclipse.microprofile.openapi.annotations.parameters.Parameter;
 import org.eclipse.microprofile.rest.client.inject.RegisterRestClient;
+
+import java.util.UUID;
 
 @Path("/api/v1/model")
 @RegisterRestClient(configKey = "model-path-api")
@@ -33,6 +38,26 @@ public interface ModelWebClient {
     @GET
     @Path("/users/search")
     @Produces(MediaType.APPLICATION_JSON)
-    public Uni<UserProfileDto> findByUserId(@NotNull @QueryParam("userId") String userId);
+    Uni<UserProfileDto> findByUserId(@NotNull @QueryParam("userId") String userId);
 
+    @GET
+    @Path("/workflow-resource/filter")
+    @Produces(MediaType.APPLICATION_JSON)
+    Uni<PageInfo<WorkflowResourceFrontEndDTO>> getWorkflowResourceFiltered(@QueryParam("pageIndex") @DefaultValue("0")
+                                                                 @Parameter(required = true, schema = @Schema(type = SchemaType.INTEGER, minimum = "0")) Integer page,
+                                                                 @QueryParam("pageSize") @DefaultValue("10")
+                                                                 @Parameter(required = true, schema = @Schema(type = SchemaType.INTEGER, minimum = "1")) Integer size,
+                                                                 @QueryParam("status")
+                                                                 @Schema(implementation = String.class, type = SchemaType.STRING, enumeration = {"CREATED", "WAITING_DEPLOY", "UPDATED_BUT_NOT_DEPLOYED", "DEPLOYED", "DEPLOY_ERROR"}) StatusEnum status,
+                                                                 @QueryParam("workflowResourceId") UUID workflowResourceId,
+                                                                 @QueryParam("deployedFileName") String deployedFileName,
+                                                                 @QueryParam("definitionKey") String definitionKey,
+                                                                 @QueryParam("resourceType") DeployableResourceType resourceType,
+                                                                 @QueryParam("sha256") String sha256,
+                                                                 @QueryParam("definitionVersionCamunda") String definitionVersionCamunda,
+                                                                 @QueryParam("camundaDefinitionId") String camundaDefinitionId,
+                                                                 @QueryParam("description") String description,
+                                                                 @QueryParam("resource") String resource,
+                                                                 @QueryParam("deploymentId") UUID deploymentId,
+                                                                 @QueryParam("fileName") String fileName);
 }
--- src/main/java/it/gov/pagopa/atmlayer/service/consolebackend/service/ModelService.java
+++ src/main/java/it/gov/pagopa/atmlayer/service/consolebackend/service/ModelService.java
@@ -3,11 +3,20 @@ package it.gov.pagopa.atmlayer.service.consolebackend.service;
 import io.smallrye.mutiny.Uni;
 import it.gov.pagopa.atmlayer.service.consolebackend.clientdto.BpmnVersionFrontEndDTO;
 import it.gov.pagopa.atmlayer.service.consolebackend.clientdto.UserProfileDto;
+import it.gov.pagopa.atmlayer.service.consolebackend.clientdto.WorkflowResourceFrontEndDTO;
+import it.gov.pagopa.atmlayer.service.consolebackend.enums.DeployableResourceType;
+import it.gov.pagopa.atmlayer.service.consolebackend.enums.StatusEnum;
 import it.gov.pagopa.atmlayer.service.consolebackend.model.PageInfo;
 
-public interface ModelService {
+import java.util.UUID;
 
+public interface ModelService {
     Uni<PageInfo<BpmnVersionFrontEndDTO>> getBpmnFiltered(int pageIndex, int pageSize, String functionType, String modelVersion, String status, String acquirerId, String filename);
 
     Uni<UserProfileDto> findByUserId(String userId);
+
+    Uni<PageInfo<WorkflowResourceFrontEndDTO>> getWorkflowResourceFiltered(int page, int pageSize, StatusEnum status, UUID workflowResourceId, String deployedFileName, String definitionKey,
+                                                                          DeployableResourceType resourceType, String sha256, String definitionVersionCamunda, String camundaDefinitionId,
+                                                                          String description, String resource, UUID deploymentId, String fileName);
+
 }
