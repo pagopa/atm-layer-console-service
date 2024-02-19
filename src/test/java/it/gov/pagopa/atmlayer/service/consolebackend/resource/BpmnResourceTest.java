43a505598875b9012ea6bdc5964734571d7bb5e4
Giacomo Brancazi <giacomo.brancazi@emeal.nttdata.com>
2024-02-19 12:25:17 +0100

    fix test

--- src/test/java/it/gov/pagopa/atmlayer/service/consolebackend/resource/BpmnResourceTest.java
+++ src/test/java/it/gov/pagopa/atmlayer/service/consolebackend/resource/BpmnResourceTest.java
@@ -25,13 +25,14 @@ import static org.mockito.Mockito.*;
 public class BpmnResourceTest {
 
     @InjectMock
-    BpmnService modelService;
+    BpmnService bpmnService;
 
     @InjectMock
     UserService userService;
 
     @Test
     void testGetAllBpmn() {
+        UUID uuid = UUID.randomUUID();
         BpmnVersionFrontEndDTO bpmnVersionFrontEndDTO = new BpmnVersionFrontEndDTO();
         List<BpmnVersionFrontEndDTO> dtoList = new ArrayList<>();
         dtoList.add(bpmnVersionFrontEndDTO);
@@ -40,17 +41,27 @@ public class BpmnResourceTest {
         userProfileDto.setProfile(UserProfileEnum.ADMIN);
         Header header = new Header("Authorization", "eyJraWQiOiJ5Uk92cjRsa2dmb3U1UURCZGM1RzI5cEtSVWxyRHVONW9oSTN2a0padXFVPSIsImFsZyI6IlJTMjU2In0.eyJhdF9oYXNoIjoiMmhBdXBnaHN3NXkyMUF3TGtxM0p0QSIsInN1YiI6Ijc2Y2U5MjYwLTMwOTEtNzAyMC00ZTMyLTU4ZmE4NDA5ZDUyYSIsImNvZ25pdG86Z3JvdXBzIjpbImV1LXNvdXRoLTFfV2ROc2hXVVNWX0dvb2dsZSJdLCJlbWFpbF92ZXJpZmllZCI6ZmFsc2UsImlzcyI6Imh0dHBzOlwvXC9jb2duaXRvLWlkcC5ldS1zb3V0aC0xLmFtYXpvbmF3cy5jb21cL2V1LXNvdXRoLTFfV2ROc2hXVVNWIiwiY29nbml0bzp1c2VybmFtZSI6Ijc2Y2U5MjYwLTMwOTEtNzAyMC00ZTMyLTU4ZmE4NDA5ZDUyYSIsIm5vbmNlIjoiNzZIVXM1d2JhX2VBZ0VIbUxrRXIyVGpQbDNuMWxvVzg3cDRfbXlfVnZhZl9oT1dyeHZkOW9vQ19oNFlvOFdlUUxsbkh0dVRBNWMzMWQybmVqbEtvanJrQ20zQkFNRlo3aE1RcGJITjZ5VDFaVXhuaE1TU1dndks5TlNrUEwxUjNGcEJFUzh1UFRNR2ZSMHljY09xU0dGSE4zazRtbjJ3eU05NWEzM0NOUjFzIiwiYXVkIjoiMzMxbHNkaXVzNDlwZDk1N245Nmdqc2wyb2siLCJpZGVudGl0aWVzIjpbeyJkYXRlQ3JlYXRlZCI6IjE3MDYxNzkxODQ4NzQiLCJ1c2VySWQiOiIxMDc4ODgxMDAyMzY3MzcwMzQ4OTUiLCJwcm92aWRlck5hbWUiOiJHb29nbGUiLCJwcm92aWRlclR5cGUiOiJHb29nbGUiLCJpc3N1ZXIiOm51bGwsInByaW1hcnkiOiJ0cnVlIn1dLCJ0b2tlbl91c2UiOiJpZCIsImF1dGhfdGltZSI6MTcwNjUyMTA2MCwiZXhwIjoxNzA2NjA3NDYwLCJpYXQiOjE3MDY1MjEwNjAsImp0aSI6IjU1YjVjNDEwLTMyZGEtNDA0ZC1hYzk1LTI4OTA3NjQ3ZTg2YyIsImVtYWlsIjoiYW50b25pby50YXJyaWNvbmVAcGFnb3BhLml0In0.OX02xa6siwktsbKbd9PH_C2xFYjpnMckipx6xLcCzx2iAxhV7ghMeDNqe1sk1mcXDxRatUexTS4nvynSlTy6wWLe81GSZTtkAvlUWQ_-mJT90BDxJp8rNC7IPqUS_4Q7QkMOuNHzh_0nYklIa_w8-sV93I3dJ61sAQUw1ye0kxu3lZ7NKUiBU07W2RH2YcdgWP4yr50s9CQMySYymPs_CP1w8eDm0vNndswP9uN4x3YRe-idQ9Q7qUPL4iZzqftahvODzU7mhTnm_IbMr9mZfEuyLNP070PeyGI7giIWdJStTZ0-8tblVAZ_DuoDkkVOHMPAL55yJTx3HRJ5beK4Uw&token_type=Bearer&expires_in=86400");
         when(userService.findByUserId(any())).thenReturn(Uni.createFrom().item(userProfileDto));
-        when(modelService.getBpmnFiltered(0, 1, "functionType", "modelVersion", "1", UUID.randomUUID(), UUID.randomUUID(), "camundaDefinitionId", "definitionKey", "deployedFileName", "reasurce", "sha256", CREATED, "acquirerId", "CREATED", "acquirerId", "fileName")).thenReturn(Uni.createFrom().item(response));
+        when(bpmnService.getBpmnFiltered(0, 1, "functionType", "modelVersion", "1", uuid, uuid, "camundaDefinitionId", "definitionKey", "deployedFileName", "resource", "sha256", CREATED, "acquirerId", "branchId", "terminalId", "fileName")).thenReturn(Uni.createFrom().item(response));
         given()
                 .header(header)
                 .queryParam("pageIndex", 0)
                 .queryParam("pageSize", 1)
                 .queryParam("functionType","functionType")
                 .queryParam("modelVersion","modelVersion")
-                .queryParam("status","CREATED")
+                .queryParam("definitionVersionCamunda","1")
+                .queryParam("bpmnId",uuid)
+                .queryParam("deploymentId",uuid)
+                .queryParam("camundaDefinitionId","camundaDefinitionId")
+                .queryParam("definitionKey","definitionKey")
+                .queryParam("deployedFileName","deployedFileName")
+                .queryParam("resource","resource")
+                .queryParam("sha256","sha256")
+                .queryParam("branchId","branchId")
+                .queryParam("terminalId","terminalId")
+                .queryParam("status",CREATED)
                 .queryParam("acquirerId","acquirerId")
                 .queryParam("fileName","fileName")
-                .when().get("/api/v1/console-backend/model/bpmn/filtred")
+                .when().get("/api/v1/console-backend/bpmn/filter")
                 .then()
                 .statusCode(200)
                 .extract()
