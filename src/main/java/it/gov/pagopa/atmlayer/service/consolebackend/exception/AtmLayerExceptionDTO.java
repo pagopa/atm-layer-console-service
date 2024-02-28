8bbe7e53a37c43d71edf7a754770351b6c564904
LuciaM1 <lucia.mazzocchi@nttdata.com>
2024-02-28 09:18:13 +0100

    adding default constructors

--- src/main/java/it/gov/pagopa/atmlayer/service/consolebackend/exception/AtmLayerExceptionDTO.java
+++ src/main/java/it/gov/pagopa/atmlayer/service/consolebackend/exception/AtmLayerExceptionDTO.java
@@ -1,10 +1,14 @@
 package it.gov.pagopa.atmlayer.service.consolebackend.exception;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class AtmLayerExceptionDTO {
     private String type;
     private String errorCode;
