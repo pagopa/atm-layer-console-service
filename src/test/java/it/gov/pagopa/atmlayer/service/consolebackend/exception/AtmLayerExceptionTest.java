ee11a16158fb17201b8c0761c55ac263e8e8657a
ElisKina-dev <elis.kina@nttdata.com>
2024-03-07 17:52:17 +0100

    junit test added

--- src/test/java/it/gov/pagopa/atmlayer/service/consolebackend/exception/AtmLayerExceptionTest.java
+++ src/test/java/it/gov/pagopa/atmlayer/service/consolebackend/exception/AtmLayerExceptionTest.java
@@ -2,6 +2,7 @@ package it.gov.pagopa.atmlayer.service.consolebackend.exception;
 
 import io.quarkus.test.junit.QuarkusTest;
 import it.gov.pagopa.atmlayer.service.consolebackend.enums.AppErrorCodeEnum;
+import jakarta.ws.rs.core.Response;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -21,5 +22,18 @@ class AtmLayerExceptionTest {
         assertEquals(500, exception.getStatusCode());
         assertEquals(AppErrorCodeEnum.ATMLCB_500.getErrorCode(), exception.getErrorCode());
         assertEquals(throwable, exception.getCause());
+    }
+
+    @Test
+    void testAtmLayerExceptionConstructor() {
+        String message = "Test message";
+        Response.Status status = Response.Status.BAD_REQUEST;
+        String type = "Test type";
+
+        AtmLayerException atmLayerException = new AtmLayerException(message, status, type);
+
+        assertEquals(message, atmLayerException.getMessage());
+        assertEquals(type, atmLayerException.getType());
+        assertEquals(status.getStatusCode(), atmLayerException.getStatusCode());
     }
 }
