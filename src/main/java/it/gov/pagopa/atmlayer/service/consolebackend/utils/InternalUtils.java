3cd9cff9f729b15b38a2e6235f1d819610626582
Giacomo Brancazi <giacomo.brancazi@emeal.nttdata.com>
2024-04-09 11:52:39 +0200

    read file from inputStream

--- src/main/java/it/gov/pagopa/atmlayer/service/consolebackend/utils/InternalUtils.java
+++ src/main/java/it/gov/pagopa/atmlayer/service/consolebackend/utils/InternalUtils.java
@@ -6,12 +6,15 @@ import jakarta.ws.rs.core.Response;
 import org.camunda.bpm.model.bpmn.Bpmn;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStream;
 
 public class InternalUtils {
 
     public static void validateBPMN(File file){
         try {
-            Bpmn.readModelFromFile(file);
+            InputStream inputStream = new FileInputStream(file);
+            Bpmn.readModelFromStream(inputStream);
         } catch (Exception e) {
             throw new AtmLayerException("Il file non  è un valido BPMN "+ (e.getCause() == null? e.getMessage() : e.getCause().getMessage()), Response.Status.NOT_ACCEPTABLE, AppErrorCodeEnum.FILE_NOT_VALID);
         }
