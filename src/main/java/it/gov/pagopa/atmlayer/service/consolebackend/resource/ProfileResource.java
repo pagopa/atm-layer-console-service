d934a199597008f5fb4e98f497d70a044871a327
LuciaM1 <lucia.mazzocchi@nttdata.com>
2024-07-22 12:19:02 +0200

    adding profile client

---
+++ src/main/java/it/gov/pagopa/atmlayer/service/consolebackend/resource/ProfileResource.java
@@ -0,0 +1,43 @@
+package it.gov.pagopa.atmlayer.service.consolebackend.resource;
+
+import io.smallrye.mutiny.Uni;
+import io.smallrye.mutiny.unchecked.Unchecked;
+import it.gov.pagopa.atmlayer.service.consolebackend.clientdto.ProfileDTO;
+import it.gov.pagopa.atmlayer.service.consolebackend.service.ProfileService;
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.inject.Inject;
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.MediaType;
+import lombok.extern.slf4j.Slf4j;
+import org.eclipse.microprofile.openapi.annotations.enums.SecuritySchemeType;
+import org.eclipse.microprofile.openapi.annotations.security.SecurityRequirement;
+import org.eclipse.microprofile.openapi.annotations.security.SecurityScheme;
+import org.eclipse.microprofile.openapi.annotations.security.SecuritySchemes;
+import org.eclipse.microprofile.openapi.annotations.tags.Tag;
+
+import java.util.List;
+
+@Path("/profile")
+@Tag(name = "Profiles", description = "Profiles proxy")
+@Slf4j
+@ApplicationScoped
+@SecuritySchemes({
+        @SecurityScheme(securitySchemeName = "bearerAuth",
+                type = SecuritySchemeType.HTTP,
+                scheme = "bearer",
+                bearerFormat = "JWT")
+})
+@SecurityRequirement(name = "bearerAuth")
+public class ProfileResource {
+
+    @Inject
+    ProfileService profileService;
+
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public Uni<List<ProfileDTO>> getAll() {
+        return this.profileService.getAll();
+    }
+}
