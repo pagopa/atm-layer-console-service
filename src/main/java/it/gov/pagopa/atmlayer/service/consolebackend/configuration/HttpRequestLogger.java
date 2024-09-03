dcaecf5a2d655f85d5f8cd1b407918b03973b9b1
EmVal <emanuele.valentini@nttdata.com>
2024-09-03 11:42:50 +0200

    Remove Autorization from logs

--- src/main/java/it/gov/pagopa/atmlayer/service/consolebackend/configuration/HttpRequestLogger.java
+++ src/main/java/it/gov/pagopa/atmlayer/service/consolebackend/configuration/HttpRequestLogger.java
@@ -6,13 +6,26 @@ import jakarta.ws.rs.ext.Provider;
 import lombok.extern.slf4j.Slf4j;
 import org.owasp.encoder.Encode;
 
+import java.util.Map;
+import java.util.List;
+
 @Provider
 @Slf4j
 public class HttpRequestLogger implements ContainerRequestFilter {
+
     public void logRequest(ContainerRequestContext requestContext) {
-        String uri = requestContext.getUriInfo().getAbsolutePath() != null ? Encode.forJava(requestContext.getUriInfo().getAbsolutePath().toString()) : null;
+        String uri = requestContext.getUriInfo().getAbsolutePath() != null
+                ? Encode.forJava(requestContext.getUriInfo().getAbsolutePath().toString())
+                : null;
+
         String method = requestContext.getMethod();
-        String headers = requestContext.getHeaders() != null ? Encode.forJava(requestContext.getHeaders().toString()) : null;
+
+        Map<String, List<String>> headersMap = requestContext.getHeaders();
+
+        headersMap.remove("Authorization");
+
+        String headers = Encode.forJava(headersMap.toString());
+
         log.info("====================================request started with, URI : {}, Method : {}, Headers  :  {}", uri, method, headers);
     }
 
