9a9f099ee9e6e0f86185de32bc6f4cb631fad46a
ElisKina-dev <144669238+ElisKina-dev@users.noreply.github.com>
2024-09-23 15:51:44 +0200

    merge dev in uat (#40)
    Co-authored-by: GitHub Action <action@github.com>
    Co-authored-by: EmVal <147710433+EmanueleValentini1@users.noreply.github.com>
    Co-authored-by: Giacomo Brancazi <giacomo.brancazi@emeal.nttdata.com>
    Co-authored-by: EmVal <emanuele.valentini@nttdata.com>
    Co-authored-by: LuciaM1 <lucia.mazzocchi@nttdata.com>
    Co-authored-by: Gabriele Maiocchi <fmaiocch@emeal.nttdata.com>

--- src/test/java/it/gov/pagopa/atmlayer/service/consolebackend/utils/HeadersUtilsTest.java
+++ src/test/java/it/gov/pagopa/atmlayer/service/consolebackend/utils/HeadersUtilsTest.java
@@ -2,8 +2,14 @@ package it.gov.pagopa.atmlayer.service.consolebackend.utils;
 
 import com.fasterxml.jackson.databind.JsonNode;
 import io.quarkus.test.junit.QuarkusTest;
+import it.gov.pagopa.atmlayer.service.consolebackend.clientdto.UserDTO;
+import it.gov.pagopa.atmlayer.service.consolebackend.enums.UserProfileEnum;
+import it.gov.pagopa.atmlayer.service.consolebackend.exception.AtmLayerException;
 import jakarta.ws.rs.container.ContainerRequestContext;
 import org.junit.jupiter.api.Test;
+
+import java.io.File;
+import java.util.ArrayList;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.mock;
@@ -53,11 +59,10 @@ class HeadersUtilsTest {
     }
 
     @Test
-    void testGetEmailJWT_NullAuthorizationHeader() {
+    void testGetEmailJWT_AccessDeniedException() {
         ContainerRequestContext containerRequestContext = mock(ContainerRequestContext.class);
-        when(containerRequestContext.getHeaderString("Authorization")).thenReturn(null);
-
-        assertThrows(NullPointerException.class, () -> HeadersUtils.getEmailJWT(containerRequestContext));
+        when(containerRequestContext.getHeaderString("Authorization")).thenReturn("test.token.extraction");
+        assertThrows(AtmLayerException.class, () -> HeadersUtils.getEmailJWT(containerRequestContext));
     }
 
     @Test
@@ -66,11 +71,31 @@ class HeadersUtilsTest {
     }
 
     @Test
-    void testGetUserIdJWT_NullAuthorizationHeader() {
+    void testGetUserIdJWT_AccessDeniedException() {
         ContainerRequestContext containerRequestContext = mock(ContainerRequestContext.class);
-        when(containerRequestContext.getHeaderString("Authorization")).thenReturn(null);
+        when(containerRequestContext.getHeaderString("Authorization")).thenReturn("test.token.extraction");
 
-        assertThrows(NullPointerException.class, () -> HeadersUtils.getUserIdJWT(containerRequestContext));
+        assertThrows(AtmLayerException.class, () -> HeadersUtils.getUserIdJWT(containerRequestContext));
+    }
+
+    @Test
+    void testHavePermission_nullProfiles() {
+        UserDTO userDTO = new UserDTO();
+        assertFalse(HeadersUtils.havePermission(userDTO, UserProfileEnum.GESTIONE_UTENTI));
+    }
+
+    @Test
+    void testHavePermission_emptyProfiles() {
+        UserDTO userDTO = new UserDTO();
+        userDTO.setProfiles(new ArrayList<>());
+        assertFalse(HeadersUtils.havePermission(userDTO, UserProfileEnum.GESTIONE_UTENTI));
+    }
+
+    @Test
+    void testFromFileToString(){
+        File file = new File("src/test/resources/Test.bpmn");
+        String expectedBase64 = "PD94bWwgdmVyc2lvbj0iMS4wIiBlbmNvZGluZz0iVVRGLTgiPz4KPGJwbW46ZGVmaW5pdGlvbnMgeG1sbnM6YnBtbj0iaHR0cDovL3d3dy5vbWcub3JnL3NwZWMvQlBNTi8yMDEwMDUyNC9NT0RFTCIgeG1sbnM6YnBtbmRpPSJodHRwOi8vd3d3Lm9tZy5vcmcvc3BlYy9CUE1OLzIwMTAwNTI0L0RJIiB4bWxuczpkYz0iaHR0cDovL3d3dy5vbWcub3JnL3NwZWMvREQvMjAxMDA1MjQvREMiIHhtbG5zOmNhbXVuZGE9Imh0dHA6Ly9jYW11bmRhLm9yZy9zY2hlbWEvMS4wL2JwbW4iIHhtbG5zOmRpPSJodHRwOi8vd3d3Lm9tZy5vcmcvc3BlYy9ERC8yMDEwMDUyNC9ESSIgeG1sbnM6bW9kZWxlcj0iaHR0cDovL2NhbXVuZGEub3JnL3NjaGVtYS9tb2RlbGVyLzEuMCIgaWQ9IkRlZmluaXRpb25zXzBqY3BqMHEiIHRhcmdldE5hbWVzcGFjZT0iaHR0cDovL2JwbW4uaW8vc2NoZW1hL2JwbW4iIGV4cG9ydGVyPSJDYW11bmRhIE1vZGVsZXIiIGV4cG9ydGVyVmVyc2lvbj0iNS4xNS4yIiBtb2RlbGVyOmV4ZWN1dGlvblBsYXRmb3JtPSJDYW11bmRhIFBsYXRmb3JtIiBtb2RlbGVyOmV4ZWN1dGlvblBsYXRmb3JtVmVyc2lvbj0iNy4xOS4wIj4KICA8YnBtbjpwcm9jZXNzIGlkPSJkZW1vMTFfMDYiIGlzRXhlY3V0YWJsZT0idHJ1ZSIgY2FtdW5kYTpoaXN0b3J5VGltZVRvTGl2ZT0iMTgwIj4KICAgIDxicG1uOnN0YXJ0RXZlbnQgaWQ9IlN0YXJ0RXZlbnRfMSI+CiAgICAgIDxicG1uOmV4dGVuc2lvbkVsZW1lbnRzPgogICAgICAgIDxjYW11bmRhOmV4ZWN1dGlvbkxpc3RlbmVyIGRlbGVnYXRlRXhwcmVzc2lvbj0iJHtwb3B1bGF0ZUJ1c2luZXNzS2V5fSIgZXZlbnQ9InN0YXJ0IiAvPgogICAgICA8L2JwbW46ZXh0ZW5zaW9uRWxlbWVudHM+CiAgICAgIDxicG1uOm91dGdvaW5nPkZsb3dfMG0xdDZjODwvYnBtbjpvdXRnb2luZz4KICAgIDwvYnBtbjpzdGFydEV2ZW50PgogICAgPGJwbW46c2VxdWVuY2VGbG93IGlkPSJGbG93XzBtMXQ2YzgiIHNvdXJjZVJlZj0iU3RhcnRFdmVudF8xIiB0YXJnZXRSZWY9InJlc3RfY2FsbCIgLz4KICAgIDxicG1uOnNlcnZpY2VUYXNrIGlkPSJyZXN0X2NhbGwiIG5hbWU9ImZpcnN0VGFzayI+CiAgICAgIDxicG1uOmV4dGVuc2lvbkVsZW1lbnRzPgogICAgICAgIDxjYW11bmRhOmNvbm5lY3Rvcj4KICAgICAgICAgIDxjYW11bmRhOmlucHV0T3V0cHV0PgogICAgICAgICAgICA8Y2FtdW5kYTppbnB1dFBhcmFtZXRlciBuYW1lPSJtZXRob2QiPkdFVDwvY2FtdW5kYTppbnB1dFBhcmFtZXRlcj4KICAgICAgICAgICAgPGNhbXVuZGE6aW5wdXRQYXJhbWV0ZXIgbmFtZT0idXJsIj5odHRwczovL3JlcXJlcy5pbi9hcGkvdXNlcnM/cGFnZT0yPC9jYW11bmRhOmlucHV0UGFyYW1ldGVyPgogICAgICAgICAgICA8Y2FtdW5kYTppbnB1dFBhcmFtZXRlciBuYW1lPSJoZWFkZXJzIj4KICAgICAgICAgICAgICA8Y2FtdW5kYTptYXA+CiAgICAgICAgICAgICAgICA8Y2FtdW5kYTplbnRyeSBrZXk9IkNvbnRlbnQtVHlwZSI+YXBwbGljYXRpb24vanNvbjwvY2FtdW5kYTplbnRyeT4KICAgICAgICAgICAgICA8L2NhbXVuZGE6bWFwPgogICAgICAgICAgICA8L2NhbXVuZGE6aW5wdXRQYXJhbWV0ZXI+CiAgICAgICAgICAgIDxjYW11bmRhOm91dHB1dFBhcmFtZXRlciBuYW1lPSJyZXMiPiR7UyhyZXNwb25zZSkucHJvcCgiZGF0YSIpfTwvY2FtdW5kYTpvdXRwdXRQYXJhbWV0ZXI+CiAgICAgICAgICA8L2NhbXVuZGE6aW5wdXRPdXRwdXQ+CiAgICAgICAgICA8Y2FtdW5kYTpjb25uZWN0b3JJZD5odHRwLWNvbm5lY3RvcjwvY2FtdW5kYTpjb25uZWN0b3JJZD4KICAgICAgICA8L2NhbXVuZGE6Y29ubmVjdG9yPgogICAgICA8L2JwbW46ZXh0ZW5zaW9uRWxlbWVudHM+CiAgICAgIDxicG1uOmluY29taW5nPkZsb3dfMG0xdDZjODwvYnBtbjppbmNvbWluZz4KICAgICAgPGJwbW46b3V0Z29pbmc+Rmxvd18wNnYycG02PC9icG1uOm91dGdvaW5nPgogICAgPC9icG1uOnNlcnZpY2VUYXNrPgogICAgPGJwbW46c2VxdWVuY2VGbG93IGlkPSJGbG93XzA2djJwbTYiIHNvdXJjZVJlZj0icmVzdF9jYWxsIiB0YXJnZXRSZWY9IkFjdGl2aXR5XzFuaTB6aTYiIC8+CiAgICA8YnBtbjp1c2VyVGFzayBpZD0iQWN0aXZpdHlfMW5pMHppNiI+CiAgICAgIDxicG1uOmluY29taW5nPkZsb3dfMDZ2MnBtNjwvYnBtbjppbmNvbWluZz4KICAgIDwvYnBtbjp1c2VyVGFzaz4KICA8L2JwbW46cHJvY2Vzcz4KICA8YnBtbmRpOkJQTU5EaWFncmFtIGlkPSJCUE1ORGlhZ3JhbV8xIj4KICAgIDxicG1uZGk6QlBNTlBsYW5lIGlkPSJCUE1OUGxhbmVfMSIgYnBtbkVsZW1lbnQ9IlByb2Nlc3NfMXF4c3ZwZCI+CiAgICAgIDxicG1uZGk6QlBNTlNoYXBlIGlkPSJfQlBNTlNoYXBlX1N0YXJ0RXZlbnRfMiIgYnBtbkVsZW1lbnQ9IlN0YXJ0RXZlbnRfMSI+CiAgICAgICAgPGRjOkJvdW5kcyB4PSIxNzkiIHk9Ijk5IiB3aWR0aD0iMzYiIGhlaWdodD0iMzYiIC8+CiAgICAgIDwvYnBtbmRpOkJQTU5TaGFwZT4KICAgICAgPGJwbW5kaTpCUE1OU2hhcGUgaWQ9IkFjdGl2aXR5XzFnZWs5a2VfZGkiIGJwbW5FbGVtZW50PSJyZXN0X2NhbGwiPgogICAgICAgIDxkYzpCb3VuZHMgeD0iMzEwIiB5PSI3NyIgd2lkdGg9IjEwMCIgaGVpZ2h0PSI4MCIgLz4KICAgICAgPC9icG1uZGk6QlBNTlNoYXBlPgogICAgICA8YnBtbmRpOkJQTU5TaGFwZSBpZD0iQWN0aXZpdHlfMTU4YW04a19kaSIgYnBtbkVsZW1lbnQ9IkFjdGl2aXR5XzFuaTB6aTYiPgogICAgICAgIDxkYzpCb3VuZHMgeD0iNzEwIiB5PSI3NyIgd2lkdGg9IjEwMCIgaGVpZ2h0PSI4MCIgLz4KICAgICAgPC9icG1uZGk6QlBNTlNoYXBlPgogICAgICA8YnBtbmRpOkJQTU5FZGdlIGlkPSJGbG93XzBtMXQ2YzhfZGkiIGJwbW5FbGVtZW50PSJGbG93XzBtMXQ2YzgiPgogICAgICAgIDxkaTp3YXlwb2ludCB4PSIyMTUiIHk9IjExNyIgLz4KICAgICAgICA8ZGk6d2F5cG9pbnQgeD0iMzEwIiB5PSIxMTciIC8+CiAgICAgIDwvYnBtbmRpOkJQTU5FZGdlPgogICAgICA8YnBtbmRpOkJQTU5FZGdlIGlkPSJGbG93XzA2djJwbTZfZGkiIGJwbW5FbGVtZW50PSJGbG93XzA2djJwbTYiPgogICAgICAgIDxkaTp3YXlwb2ludCB4PSI0MTAiIHk9IjExNyIgLz4KICAgICAgICA8ZGk6d2F5cG9pbnQgeD0iNzEwIiB5PSIxMTciIC8+CiAgICAgIDwvYnBtbmRpOkJQTU5FZGdlPgogICAgPC9icG1uZGk6QlBNTlBsYW5lPgogIDwvYnBtbmRpOkJQTU5EaWFncmFtPgo8L2JwbW46ZGVmaW5pdGlvbnM+Cg==";
+        assertEquals(expectedBase64, HeadersUtils.fromFileToString(file));
     }
 
 }
