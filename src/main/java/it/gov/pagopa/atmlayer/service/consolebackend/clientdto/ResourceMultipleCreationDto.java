4ec1d4233439770ab78ad12f077c2992cc6de74e
LuciaM1 <lucia.mazzocchi@nttdata.com>
2024-09-30 15:26:35 +0200

    adding input length constraints

--- src/main/java/it/gov/pagopa/atmlayer/service/consolebackend/clientdto/ResourceMultipleCreationDto.java
+++ src/main/java/it/gov/pagopa/atmlayer/service/consolebackend/clientdto/ResourceMultipleCreationDto.java
@@ -10,6 +10,7 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.eclipse.microprofile.openapi.annotations.enums.SchemaType;
 import org.eclipse.microprofile.openapi.annotations.media.Schema;
+import org.hibernate.validator.constraints.Length;
 import org.jboss.resteasy.reactive.PartType;
 
 import java.io.File;
@@ -42,9 +43,11 @@ public class ResourceMultipleCreationDto {
     @Schema(description = "Description of the path parameter: example/path",
             pattern = "(^$)|(^(?!/)[a-zA-Z0-9/]+(?<!/)$)", format = "byte", maxLength = 255)
     @PartType("text/plain")
+    @Length(max = 150)
     private String path;
     @FormParam("description")
     @PartType("text/plain")
     @Schema(format = "byte", maxLength = 255)
+    @Length(max = 255)
     private String description;
 }
